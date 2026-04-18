7e924ef075027df0e3fb520b6ea45e2c930c79f9
Emiliano Ruiz Plancarte <emiliano.rplancarte@hotmail.com>
2026-04-18 12:07:33 -0600

    Initial Commit

---
+++ LoveSpeaks/LoveSpeaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveSpeaksApp.swift
+//  LoveSpeaks
+//
+//  Created by Emiliano Ruíz Plancarte on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LoveSpeaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoveSpeaksTests/LoveSpeaksTests.swift
@@ -0,0 +1,19 @@
+//
+//  LoveSpeaksTests.swift
+//  LoveSpeaksTests
+//
+//  Created by Emiliano Ruíz Plancarte on 18/04/26.
+//
+
+import Testing
+@testable import LoveSpeaks
+
+struct LoveSpeaksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
