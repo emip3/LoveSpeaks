0959b0e038f9ec2b74c284305173573d140ea0a6
Emiliano Ruiz Plancarte <emiliano.rplancarte@hotmail.com>
2026-04-18 12:48:41 -0600

    primer commit

---
+++ LoveSpeaks/Views/ConfigView.swift
@@ -0,0 +1,10 @@
+//
+//  ConfigView.swift
+//  LoveSpeaks
+//
+//  Created by Emiliano Ruíz Plancarte on 18/04/26.
+//
+
+// Configuración general de la app
+
+// TODO: 
---
+++ LoveSpeaks/Views/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  LoveSpeaks
+//
+//  Created by Emiliano Ruíz Plancarte on 18/04/26.
+//
+
+// Perfil del padre
+
+// TODO: Gráfica "Índice de confianza"
+
+// TODO: Ritmo cardiáco del padre (detectar estrés)
+
+// TODO: Gráfica de sueño
+
+// TODO: Botón de "DESCANSO" para activar solo las notificaciones de alerta.
